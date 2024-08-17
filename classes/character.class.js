60d6b5fa40cdf1ced0192387bdb22b89646fe7c7
Deividas Kondratjevas <deividaskond@gmail.com>
2024-08-17 12:57:13 +0200

    pepe moves

--- classes/character.class.js
+++ classes/character.class.js
@@ -1,6 +1,7 @@
 class Charackter extends MovableObject {
   height = 270;
   width = 100;
+  speed = 10;
 
   IMAGES_WALKING = [
     "img/2_character_pepe/2_walk/W-21.png",
@@ -11,7 +12,7 @@ class Charackter extends MovableObject {
     "img/2_character_pepe/2_walk/W-26.png",
   ];
 
-  
+  world;
 
   constructor() {
     super().loadImage("img/2_character_pepe/2_walk/W-21.png");
@@ -21,13 +22,34 @@ class Charackter extends MovableObject {
   }
 
   animate() {
+
     setInterval(() => {
-      let i = this.currentImage % this.IMAGES_WALKING.length; 
-      // i = 0, 1, 2, 3, 4, 5, 0
-      let path = this.IMAGES_WALKING[i];
-      this.img = this.imageCache[path];
-      this.currentImage++;
-    }, 100);
+      if (this.world.keyboard.RIGHT) {
+        this.x += this.speed;
+        this.otherDirection = false;
+
+      }
+      if (this.world.keyboard.LEFT) {
+        this.x -= this.speed;
+        this.otherDirection = true;
+
+      }
+
+    }, 1000 / 60);
+
+
+    setInterval(() => {
+      if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+
+
+        //Walk animation
+        let i = this.currentImage % this.IMAGES_WALKING.length;
+        // i = 0, 1, 2, 3, 4, 5, 0
+        let path = this.IMAGES_WALKING[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+      }
+    }, 50);
   }
 
   jump() {}
