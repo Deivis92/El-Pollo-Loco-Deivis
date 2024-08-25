ea3bdc9450ca5b53e1b7851864ecc8c97ce53886
Deividas Kondratjevas <deividaskond@gmail.com>
2024-08-25 10:44:12 +0200

    cloud moving

--- classes/cloud.class.js
+++ classes/cloud.class.js
@@ -2,16 +2,19 @@ class Cloud extends MovableObject {
   y = 20;
   height = 240;
   width = 500;
-  
+  speed;
 
   constructor() {
     super().loadImage("./img/5_background/layers/4_clouds/1.png");
-
     this.x = Math.random() * 1000;
+    this.speed = 0.15; // Set the speed for the cloud to move left
     this.animate();
+
   }
 
   animate() {
-    this.moveLeft();
+    setInterval(() => {
+      this.moveLeft(); 
+    }, 1000 / 60); 
   }
 }
